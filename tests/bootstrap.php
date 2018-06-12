099de7057bd5ac3df6291a27ba1c9c1211ccbd02
pogla <matic.pogladic@gmail.com>
2018-06-12 09:05:33 +0200

    Remove install

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,130 +1,94 @@
 <?php
 /**
- * WooCommerce Unit Tests Bootstrap
+ * Class WC_Test_Plugin_Tests_Bootstrap
+ *
+ * PHPUnit bootstrap file
+ *
+ * @package Woocommerce_Test_Plugin
  */
-class WC_Plugin_Tests_Bootstrap {
 
-	/** @var WC_Plugin_Tests_Bootstrap instance */
-	protected static $instance = null;
+class WC_Test_Plugin_Tests_Bootstrap {
 
-	/** @var string directory where wordpress-tests-lib is installed */
-	public $wp_tests_dir;
-
-	/** @var string testing directory */
-	public $tests_dir;
-
-	/** @var string plugin directory */
-	public $plugin_dir;
-
-	public $wc_plugin_dir;
-
-	public $wc_tests_dir;
-
-	public $woo_tests_dir;
+	public $wpTestsDir  = null;
+	public $testsDir    = null;
+	public $pluginDir   = null;
+	public $modulesDir   = null;
+	public $wcTestsDir   = null;
 
 	/**
-	 * Setup the unit testing environment.
+	 * Constructor to the bootstrap file
 	 */
 	public function __construct() {
 
-		ini_set( 'display_errors','on' );
-		error_reporting( E_ALL );
-
-		// Ensure server variable is set for WP email functions.
 		if ( ! isset( $_SERVER['SERVER_NAME'] ) ) {
 			$_SERVER['SERVER_NAME'] = 'localhost';
 		}
 
-		$this->tests_dir     = dirname( __FILE__ );
-		$this->plugin_dir    = dirname( $this->tests_dir );
-		$this->wp_tests_dir  = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) :  $this->plugin_dir . '/tmp/wordpress-tests-lib';
-		$this->woo_tests_dir  = getenv( 'WOO_TESTS_DIR' ) ? getenv( 'WOO_TESTS_DIR' ) :  $this->plugin_dir . '/tmp/woocommerce';
-		$this->wc_plugin_dir = $this->woo_tests_dir . '/';
-		$this->wc_tests_dir  = $this->wc_plugin_dir . 'tests';
+		if ( getenv( 'WP_MULTISITE' ) ) {
+			define( 'WP_TESTS_MULTISITE', 1 );
+		}
+
+		if ( ! defined( 'WC_UNIT_TESTING' ) ) {
+			define( 'WC_UNIT_TESTING', true );
+		}
+
+		$this->testsDir = dirname(__FILE__);
+		$this->pluginDir = dirname($this->testsDir);
+
+		$this->wpTestsDir = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : '/tmp/wordpress-tests-lib';
+		$this->wcTestsDir = getenv( 'WC_TESTS_DIR' ) ? getenv( 'WC_TESTS_DIR' ) : '/tmp/woocommerce';
+
+		var_dump('PHP testsDir:');
+		var_dump($this->testsDir);
+		var_dump('PHP pluginDir:');
+		var_dump($this->pluginDir);
+		var_dump('PHP wpTestsDir:');
+		var_dump($this->wpTestsDir);
 
 		// load test function so tests_add_filter() is available
-		require_once( $this->wp_tests_dir . '/includes/functions.php' );
+		include_once $this->wpTestsDir . '/includes/functions.php';
 
-		// load WC
-		tests_add_filter( 'muplugins_loaded', array( $this, 'load_wc_test_plugin' ) );
+		// Load WooCommerce
+		tests_add_filter( 'muplugins_loaded', array( $this, 'loadWooCommerce' ) );
 
-		// install WC
-		tests_add_filter( 'init', array( $this, 'install_wc_test_plugin' ) );
+		// Load the plugin
+		tests_add_filter( 'muplugins_loaded', array( $this, 'loadPlugin' ) );
 
-		// load the WP testing environment
-		require_once( $this->wp_tests_dir . '/includes/bootstrap.php' );
-		// load WC testing framework
+		// Start up the WP testing environment.
+		include_once $this->wpTestsDir . '/includes/bootstrap.php';
+
+		// Include utility classes
 		$this->includes();
+		
 	}
 
 	/**
-	 * Load WooCommerce.
+	 * Loads the plugin for the current PHPUnit runtime
 	 *
-	 * @since 2.2
+	 * @return void
 	 */
-	public function load_wc_test_plugin() {
-		require_once( $this->wc_plugin_dir . '/woocommerce.php' );
-		require_once( $this->plugin_dir . '/woocommerce-test-plugin.php' );
+	public function loadPlugin() {
+		include_once $this->pluginDir . '/woocommerce-test-plugin.php';
 	}
 
 	/**
-	 * Install WooCommerce after the test environment and WC have been loaded.
+	 * Loads the WooCommerce plugin for the current PHPUnit runtime
 	 *
-	 * @since 2.2
+	 * @return void
 	 */
-	public function install_wc_test_plugin() {
-
-		global $wp_roles;
-
-		// clean existing install first
-		define( 'WP_UNINSTALL_PLUGIN', true );
-		define( 'WC_REMOVE_ALL_DATA', true );
-
-		include( $this->wc_plugin_dir . '/uninstall.php' );
-
-		WC_Install::install();
-
-		require_once( $this->plugin_dir . '/woocommerce-test-plugin.php' );
-
-		// reload capabilities after install, see https://core.trac.wordpress.org/ticket/28374
-		$wp_roles = new WP_Roles();
-
+	public function loadWooCommerce() {
+		require_once $this->wcTestsDir . '/woocommerce.php';
 	}
+
 	/**
-	 * Load WC-specific test cases and factories.
-	 *
-	 * @since 2.2
+	 * Loads the required files.
 	 */
-	public function includes() {
-		// factories
-		require_once( $this->tests_dir . '/framework/factories/class-wc-gzd-unit-test-factory-for-webhook.php' );
-		require_once( $this->tests_dir . '/framework/factories/class-wc-gzd-unit-test-factory-for-webhook-delivery.php' );
-		// framework
-		require_once( $this->tests_dir . '/framework/class-wc-gzd-unit-test-factory.php' );
-		require_once( $this->tests_dir . '/framework/vendor/class-wp-test-spy-rest-server.php' );
-		// test cases
-		require_once( $this->tests_dir . '/framework/class-wc-gzd-unit-test-case.php' );
-		require_once( $this->tests_dir . '/framework/class-wc-gzd-rest-unit-test-case.php' );
-		// Helpers
-		require_once( $this->wc_tests_dir . '/framework/helpers/class-wc-helper-product.php' );
-		require_once( $this->tests_dir . '/framework/helpers/class-wc-gzd-helper-product.php' );
-		require_once( $this->wc_tests_dir . '/framework/helpers/class-wc-helper-customer.php' );
-		require_once( $this->tests_dir . '/framework/helpers/class-wc-gzd-helper-customer.php' );
-		require_once( $this->wc_tests_dir . '/framework/helpers/class-wc-helper-shipping.php' );
-		require_once( $this->wc_tests_dir . '/framework/helpers/class-wc-helper-order.php' );
-		require_once( $this->tests_dir . '/framework/helpers/class-wc-gzd-helper-order.php' );
-	}
-	/**
-	 * Get the single class instance.
-	 *
-	 * @since 2.2
-	 * @return WC_Plugin_Tests_Bootstrap
-	 */
-	public static function instance() {
-		if ( is_null( self::$instance ) ) {
-			self::$instance = new self();
-		}
-		return self::$instance;
+	private function includes() {
+		// Load WC Helper functions/Frameworks and Factories.
+		require_once( $this->wcTestsDir . '/tests/framework/helpers/class-wc-helper-product.php' );
+		require_once( $this->wcTestsDir . '/tests/framework/helpers/class-wc-helper-order.php' );
+		require_once( $this->wcTestsDir . '/tests/framework/helpers/class-wc-helper-shipping.php' );
 	}
 }
-WC_Plugin_Tests_Bootstrap::instance();
+
+new WC_Test_Plugin_Tests_Bootstrap();
